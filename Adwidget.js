3dd8d2e244143bdc5850e671bbd81cc769451434
bsmjsambalpuri <83930752+bsmjsambalpuri@users.noreply.github.com>
2021-12-14 12:27:36 +0530

    Update Adwidget.js

--- Adwidget.js
+++ Adwidget.js
@@ -1,19 +1,4 @@
-<!--Random Ad Widget-->
-
-<style>
-.main-Ad {
-  background-color: white;
-  text-align: left;
-  width: 100%;
-  padding: 15px;
-  box-sizing: border-box;
-  box-shadow:0px 1px 2px grey;
-  border-left: 10px solid #f24a4a;
-}
-</style>
-<div class='main-Ad'>&#9193; <b>
-<script type='text/javascript'>
- var AdLinks=new Array();
+var AdLinks=new Array();
 
  AdLinks[0]="<a href='https://play.google.com/store/apps/details?id=com.bsmj.sambalpuridesign'>Download Sambalpuri Name Design - DP APP </a>";
 
@@ -25,5 +10,3 @@
 
  var random=Math.floor(4*Math.random());
  document.write(AdLinks[random]);
- </script>
-  </b></div><br/>
